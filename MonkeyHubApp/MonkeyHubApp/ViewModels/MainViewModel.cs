6d666979601946fb0bc0f9a72b6f38583b9edba5
Anderson Pereira <anderson.rasec@outlook.com>
2017-05-20 20:16:11 -0300

    Model 2. Terminado o Binding.

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,40 @@
-﻿namespace MonkeyHubApp.ViewModels
-{
-    public class MainViewModel
-    {
-        public string Descricao { get; set; }
+﻿using System.Threading.Tasks;
 
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _descricao;
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            set { SetProperty(ref _descricao, value); }
+        }
+
+        private int _idade;
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { SetProperty(ref _idade, value);
+}
+        }
+        
         public MainViewModel()
         {
-            Descricao = "Olá mundo!";
+            Descricao = "Olá mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(async t =>
+            {
+                Descricao = "Meu texto mudou";
+
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    Descricao = $"Meu texto mudou {i}";
+                }
+            });
         }
     }
 }
