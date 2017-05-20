d1fb53ab20afcbf7266e07e7edd4f9cc8433c631
Anderson Pereira <anderson.rasec@outlook.com>
2017-05-20 20:33:10 -0300

    Model 2. Commands e Alerts. Clean the MainViewModel.

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -4,37 +4,6 @@ namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        private string _descricao;
-
-        public string Descricao
-        {
-            get { return _descricao; }
-            set { SetProperty(ref _descricao, value); }
-        }
-
-        private int _idade;
-
-        public int Idade
-        {
-            get { return _idade; }
-            set { SetProperty(ref _idade, value);
-}
-        }
-        
-        public MainViewModel()
-        {
-            Descricao = "Olá mundo! Eu estou aqui!";
-
-            Task.Delay(3000).ContinueWith(async t =>
-            {
-                Descricao = "Meu texto mudou";
-
-                for (int i = 0; i < 10; i++)
-                {
-                    await Task.Delay(1000);
-                    Descricao = $"Meu texto mudou {i}";
-                }
-            });
-        }
+      
     }
 }
