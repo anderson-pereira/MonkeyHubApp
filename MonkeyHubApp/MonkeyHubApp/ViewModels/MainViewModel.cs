3575a9427bedafb299e1dc04b11ae95d674b2f7f
Anderson Pereira <anderson.rasec@outlook.com>
2017-05-21 00:33:43 -0300

    Model 2. Commands e Alerts. Nova implementação da MainPage com Command.

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,9 +1,30 @@
 ﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-      
+        private string _searchTerm;     
+
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set { SetProperty(ref _searchTerm, value); }
+        }
+
+        //Não precisa ser Binding, pois a instância inicial não mudará.
+        public Command SearchCommand { get; } //get only, Só è modificada no construtor.
+
+        public MainViewModel()
+        {
+            SearchCommand = new Command(ExecuteSearchCommand);
+        }
+
+        void ExecuteSearchCommand()
+        {
+
+        }
+
     }
 }
